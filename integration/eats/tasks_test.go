dc86d659bcd60365539ac1e2c9112fb90af4a57d
Kieron Browne <kbrowne@pivotal.io>
2020-05-12 15:05:47 +0100

    improve eats
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- integration/eats/tasks_test.go
+++ integration/eats/tasks_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"time"
 
 	"code.cloudfoundry.org/eirini"
 	"code.cloudfoundry.org/eirini/integration/util"
@@ -13,6 +12,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
+	batchv1 "k8s.io/api/batch/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -49,26 +49,40 @@ var _ = Describe("Tasks", func() {
 
 	Context("When an task is created", func() {
 		BeforeEach(func() {
-			task = cf.TaskRequest{
-				GUID: "the-task",
-				Lifecycle: cf.Lifecycle{
-					BuildpackLifecycle: &cf.BuildpackLifecycle{
-						StartCommand: "fubar",
-					},
-				},
-			}
-			resp, err := desireTask(httpClient, opiURL, task)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(resp.StatusCode).To(Equal(http.StatusAccepted))
 		})
 
 		It("cleans up the job after it completes", func() {
-			time.Sleep(10 * time.Second)
-			jobs, err := fixture.Clientset.BatchV1().Jobs(fixture.Namespace).List(metav1.ListOptions{
-				LabelSelector: fmt.Sprintf("%s=%s, %s=%s", k8s.LabelSourceType, "TASK", k8s.LabelGUID, "the-task"),
+			By("creating the task", func() {
+				task = cf.TaskRequest{
+					GUID: "the-task",
+					Lifecycle: cf.Lifecycle{
+						DockerLifecycle: &cf.DockerLifecycle{
+							Image:   "busybox",
+							Command: []string{"sh", "-c", "sleep 1; echo something to stdout"},
+						},
+					},
+				}
+				resp, err := desireTask(httpClient, opiURL, task)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(resp.StatusCode).To(Equal(http.StatusAccepted))
+
+				Eventually(getTaskJobsFn("the-task"), "10s").Should(HaveLen(1))
+
+				// TODO: assert stdout content matches once this is available in EATS
 			})
-			Expect(err).NotTo(HaveOccurred())
-			Expect(jobs.Items).To(BeEmpty())
+
+			By("cleaning up the task", func() {
+				Eventually(getTaskJobsFn("the-task"), "10s").Should(BeEmpty())
+			})
 		})
 	})
 })
+
+func getTaskJobsFn(guid string) func() ([]batchv1.Job, error) {
+	return func() ([]batchv1.Job, error) {
+		jobs, err := fixture.Clientset.BatchV1().Jobs(fixture.Namespace).List(metav1.ListOptions{
+			LabelSelector: fmt.Sprintf("%s=%s, %s=%s", k8s.LabelSourceType, "TASK", k8s.LabelGUID, guid),
+		})
+		return jobs.Items, err
+	}
+}
