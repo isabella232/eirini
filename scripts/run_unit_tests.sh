86d754d6c8b1869c1f2609c3495152eb24eb8ff4
Dimitris Karakasilis <DKarakasilis@suse.com>
2020-11-27 16:30:34 +0200

    Generate coverage results when running unit tests

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -12,7 +12,11 @@ main() {
 
 run_tests() {
   pushd "$BASEDIR" >/dev/null || exit 1
-  ginkgo -mod=vendor -p -r -keepGoing --skipPackage=tests -randomizeAllSpecs -randomizeSuites "$@"
+  mkdir -p coverage
+  # TODO: Find a way to not generate multiple files other than the one in the `coverage/` dir
+  # If there is no way, cleanup with something like: `find -name *coverage.out -exec rm {} \;`
+  # After moving the file in the `coverage` dir to some other name that doesn't match the above.
+  ginkgo -cover -outputdir="coverage/" -coverprofile=coverage.out -mod=vendor -p -r -keepGoing --skipPackage=tests -randomizeAllSpecs -randomizeSuites "$@"
   popd >/dev/null || exit 1
 }
 
